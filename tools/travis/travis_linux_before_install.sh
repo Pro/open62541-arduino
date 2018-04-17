c8696c3ebd520f3c1df3bed4f36f2fffb2a9c4f7
Stefan Profanter <git@s.profanter.me>
2018-04-17 13:50:28 +0200

    Use ARDUINO_SDK_PATH environment variable

--- tools/travis/travis_linux_before_install.sh
+++ tools/travis/travis_linux_before_install.sh
@@ -11,4 +11,8 @@ mkdir espressif && cd espressif
 git clone https://github.com/espressif/arduino-esp32 esp32
 cd esp32
 git submodule update --init --recursive
-python tools/get.py
+cd tools && python get.py
+cd ..
+
+# Increase the stack size
+sed -i 's/xTaskCreatePinnedToCore(loopTask, "loopTask", 8192, NULL, 1, NULL, ARDUINO_RUNNING_CORE);/xTaskCreatePinnedToCore(loopTask, "loopTask", 16384, NULL, 1, NULL, ARDUINO_RUNNING_CORE);/g' cores/esp32/main.cpp
