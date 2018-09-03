4dd4bf3b398512afcb1e7e6eebf34ee139e46562
Stefan Profanter <git@s.profanter.me>
2018-09-03 17:57:50 +0200

    Update to Arduino SDK 1.8.6 and fix build errors using current master esp32

--- tools/travis/travis_linux_before_install.sh
+++ tools/travis/travis_linux_before_install.sh
@@ -2,16 +2,14 @@
 set -ev
 
 # Get arduino sdk
-wget https://downloads.arduino.cc/arduino-1.8.5-linux64.tar.xz
-tar xf arduino-1.8.5-linux64.tar.xz
-cd arduino-1.8.5/hardware
+wget https://downloads.arduino.cc/arduino-1.8.6-linux64.tar.xz
+tar xf arduino-1.8.6-linux64.tar.xz
+cd arduino-1.8.6/hardware
 
 # Additionally get the ESP toolchain
 mkdir espressif && cd espressif
 
-# Use custom version until https://github.com/espressif/arduino-esp32/pull/1326 is fixed
-#git clone --depth 1 https://github.com/espressif/arduino-esp32 esp32
-git clone --depth 1 https://github.com/Pro/arduino-esp32 -b hotfix/asm esp32
+git clone --depth 1 https://github.com/espressif/arduino-esp32 esp32
 
 cd esp32
 
